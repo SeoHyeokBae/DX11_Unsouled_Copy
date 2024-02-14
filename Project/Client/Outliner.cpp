86ddaa7b165af9465abafd5d452a46578622ddde
SeoHyeokBae <pear.vero@gmail.com>
2024-02-14 20:31:48 +0900

    AddChild 관련 문제 수정

--- Project/Client/Outliner.cpp
+++ Project/Client/Outliner.cpp
@@ -89,10 +89,18 @@ void Outliner::DragDropObject(DWORD_PTR _Dest, DWORD_PTR _Source)
     TreeNode* pDestNode = (TreeNode*)_Dest;
     TreeNode* pSourceNode = (TreeNode*)_Source;
 
+    CGameObject* pDestObj = nullptr;
+    if (nullptr != pDestNode)
+        pDestObj = (CGameObject*)pDestNode->GetData();
+
+    CGameObject* pSourceObj = (CGameObject*)pSourceNode->GetData();
+
+    // 부모 오브젝트가 자신의 자식오브젝트의 자식으로 들어가려는 경우는 방지
+    if (pDestObj != nullptr && pDestObj->IsAncestor(pSourceObj))
+        return;
+
     if (nullptr == pDestNode)
     {
-        CGameObject* pSourceObj = (CGameObject*)pSourceNode->GetData();
-
         int LayerIdx = pSourceObj->DisconnectWithParent();
 
         // 원래 부모가 없었다
@@ -104,8 +112,6 @@ void Outliner::DragDropObject(DWORD_PTR _Dest, DWORD_PTR _Source)
     }
     else
     {
-        CGameObject* pDestObj = (CGameObject*)pDestNode->GetData();
-        CGameObject* pSourceObj = (CGameObject*)pSourceNode->GetData();
         pDestObj->AddChild(pSourceObj);
     }
 
